6e459f723b63c2f47339d69f9200eea483f2e41e
jmg2027 <132035258+jmg2027@users.noreply.github.com>
2025-06-27 19:43:20 +0900

    Update package.scala

--- spec-core/src/main/scala/spec/core/package.scala
+++ spec-core/src/main/scala/spec/core/package.scala
@@ -1,13 +1,17 @@
 package spec.core
+import upickle.default.{ReadWriter, macroRW}
 
-sealed trait TagCat { def prefix: String }
-object TagCat {
-  case object CONTRACT extends TagCat { val prefix = "CONTRACT" }
-  case object FUNC     extends TagCat { val prefix = "FUNC"     }
-  case object PROP     extends TagCat { val prefix = "PROP"     }
-  case object COV      extends TagCat { val prefix = "COV"      }
-  case object PARAM    extends TagCat { val prefix = "PARAM"    }
-  final case class Raw(prefix: String) extends TagCat
+sealed trait SpecCategory { def prefix: String }
+object SpecCategory {
+  case object CONTRACT extends SpecCategory { val prefix = "CONTRACT" }
+  case object FUNC     extends SpecCategory { val prefix = "FUNC"     }
+  case object PROP     extends SpecCategory { val prefix = "PROP"     }
+  case object COV      extends SpecCategory { val prefix = "COV"      }
+  case object PARAM    extends SpecCategory { val prefix = "PARAM"    }
+
+  final case class Raw(prefix: String) extends SpecCategory
+
+//  implicit def rw: ReadWriter[SpecCategory] = macroRW[SpecCategory]
 }
 
 sealed trait Capability ; object Capability {
@@ -15,6 +19,7 @@ sealed trait Capability ; object Capability {
   case object LOWPWR_QUEUE extends Capability { val name = "LOWPWR_QUEUE" }
   case object EXT_IP       extends Capability { val name = "EXT_IP"       }
   final case class Raw(name: String) extends Capability
+  implicit val rw: ReadWriter[Capability] = macroRW
 }
 
 trait HardwareSpecification {
